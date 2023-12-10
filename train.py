5b098ea417b5d8f2081159b3958bcfba6d298e7b
Gabriel <rafael@MacBook-Pro-Rafael.local>
2023-12-11 00:56:24 +0100

    Added Checkpoints and EarlyStopping to the training loop

--- train.py
+++ train.py
@@ -11,9 +11,11 @@ from itertools import groupby
 
 import datetime
 
+import csv
 
 
-def train(type, name):
+
+def train(type, name, rate):
 
 	print("Hola")
 	
@@ -22,23 +24,35 @@ def train(type, name):
 	print(w2i)
 	print('---------------------------')
 	print()
+
+	if rate is not None:
+		data.create_fold(type = type, name = name, partition = "train", rate = rate)
+		data.create_fold(type = type, name = name, partition = "valid", rate = rate)
 	
 	# Create model:
 	crnn = model.CRNN(height = config.img_height, num_classes = len(w2i))
 
 	# Create data generator:
-	data_gen = data.data_generator(w2i = w2i, type = type, name = name, img_height = config.img_height)
+	data_gen = data.data_generator(w2i = w2i, type = type, name = name, rate = rate, img_height = config.img_height)
 
 	# Instantiating CTC loss:
-	ctc_loss = nn.CTCLoss(blank = len(w2i))
+	ctc_loss = nn.CTCLoss(blank = len(w2i), zero_infinity=True)
 
 	# ADAM optimizer:
 	optimizer = torch.optim.Adam(crnn.parameters(), lr=0.001)
 
+	best_SER = float.MAX_VALUE
+	count = 0
+
 	# Fitting model:
 	#with open(os.path.join(config.folds_path, 'Fold' + str(config.fold), 'Partitions', 'Train.lst')) as f:
-	with open(os.path.join(config.folds_path, type, name, 'train.txt')) as f:
+	#with open(os.path.join(config.folds_path, type, name, 'train.txt')) as f:
+	#	total_files = len(list(map(str.strip, f.readlines())))
+	#	print(total_files)
+
+	with open(os.path.join(config.folds_path, type, name, rate, 'train.txt')) as f:
 		total_files = len(list(map(str.strip, f.readlines())))
+		print(total_files)
 	
 	# Run on GPU
 	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -46,6 +60,13 @@ def train(type, name):
 	crnn.to(device)
 	print(f"Device {device}")
 
+	if not os.path.exists('Modelos'):
+   		# Create a new directory because it does not exist
+		os.makedirs('Modelos')
+
+	if not os.path.exists(os.path.join('Modelos', name)):
+   		# Create a new directory because it does not exist
+		os.makedirs(os.path.join('Modelos', name))
 
 	# Train loop:
 	loss_acc = []
@@ -80,24 +101,28 @@ def train(type, name):
 		# Evaluating model:
 		crnn.eval()
 		###-Validation:
-		SER_val = evalute_partition(w2i = w2i, pred_model = crnn, type = type, name = name, img_height = config.img_height, partition = 'valid')
+		SER_val = evalute_partition(w2i = w2i, pred_model = crnn, type = type, name = name, rate = rate, img_height = config.img_height, partition = 'valid')
 		#SER_val = evalute_partition(w2i = w2i, pred_model = crnn, img_height = config.img_height, partition = 'ValidationLines')
 
 		###-Test:
-		SER_test = evalute_partition(w2i = w2i, pred_model = crnn, type = type, name = name, img_height = config.img_height, partition = 'test')
+		#SER_test = evalute_partition(w2i = w2i, pred_model = crnn, type = type, name = name, img_height = config.img_height, partition = 'test')
 		#SER_test = evalute_partition(w2i = w2i, pred_model = crnn, img_height = config.img_height, partition = 'TestLines')
-		print("Epoch #{}\tVAL: {:.2f}%\tTEST: {:.2f}%".format(it_epoch+1, 100*SER_val, 100*SER_test))
+		#print("Epoch #{}\tVAL: {:.2f}%\tTEST: {:.2f}%".format(it_epoch+1, 100*SER_val, 100*SER_test))
 
-	if not os.path.exists('Modelos'):
-   		# Create a new directory because it does not exist
-		os.makedirs('Modelos')
-
-	if not os.path.exists(os.path.join('Modelos', name)):
-   		# Create a new directory because it does not exist
-		os.makedirs(os.path.join('Modelos', name))
+		print("Epoch #{}\tVAL: {:.2f}%".format(it_epoch+1, 100*SER_val))
 		   
-	PATH = os.path.join('Modelos', name, datetime.date.today().strftime("%m-%d-%Y_%H-%M-%S") + '.pt') 
-	torch.save(crnn, PATH)
+		# Checkpoint
+		if SER_val < best_SER:
+			PATH = os.path.join('Modelos', name, datetime.date.today().strftime("%m-%d-%Y_%H-%M-%S") + '.pt')
+			torch.save(crnn, PATH)
+			count = 0
+		else:
+			count += 1
+
+		# EarlyStopping
+		if count == config.patience:
+			print('Training Stopped: EarlyStopping')
+			break
 
 	return
 
@@ -106,6 +131,10 @@ def train(type, name):
 
 def decode_CTC(batch_posteriorgram, input_length):
 	out = list()
+	confidences = list()
+
+	#print("Lenght: ", len(batch_posteriorgram))
+	#print("Lenght: ", len(input_length))
 
 	for it_batch in range(len(batch_posteriorgram)):
 		#Performing Best Path decoding:
@@ -135,13 +164,21 @@ def decode_CTC(batch_posteriorgram, input_length):
 		#print(decoded_sequence)
 
 		out.append(decoded_sequence)
-	return out
+		confidences.append(mean_estimation)
+
+	return out, confidences
 
 
 
-def evalute_partition(w2i, pred_model, type, name, img_height = 50, partition = 'Test'):
+def evalute_partition(w2i, pred_model, type, name, rate, img_height = 50, partition = 'Test'):
 	#with open(os.path.join(config.folds_path, 'Fold' + str(config.fold), 'Partitions', partition + '.lst')) as f:
-	with open(os.path.join(config.folds_path, type, name, partition + '.txt')) as f:
+	new_name = None
+	if rate is not None:
+		new_name = name + '/' + rate
+	else:
+		new_name = name
+
+	with open(os.path.join(config.folds_path, type, new_name, partition + '.txt')) as f:
 		files = list(map(str.strip, f.readlines()))
 
 	device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -150,14 +187,22 @@ def evalute_partition(w2i, pred_model, type, name, img_height = 50, partition = 
 	it = 0
 	y_true = list()
 	y_pred = list()
+
+	files_names = list()
+	confidences = list()
+	
 	while it < len(files):
 		init_idx = it
 		end_idx = min(it + config.batch_size, len(files))
 		img_batch, gt_batch, input_length, _ = data.load_batch_data(w2i, files[init_idx:end_idx], img_height, type, name)
 
+		#print("Lenght: ", len(img_batch))
 
 		batch_posteriorgram = pred_model(torch.from_numpy(np.array(img_batch)).float().to(device))
-		prediction = decode_CTC(batch_posteriorgram.cpu().detach().numpy(), input_length)
+		prediction, confidence_list = decode_CTC(batch_posteriorgram.cpu().detach().numpy(), input_length)
+
+		files_names += files[init_idx:end_idx]
+		confidences += confidence_list
 
 		#print("Prediction: ", prediction)
 		#print("True      : ", gt_batch)
@@ -169,7 +214,29 @@ def evalute_partition(w2i, pred_model, type, name, img_height = 50, partition = 
 
 	ser = evaluation.SER(y_true = y_true, y_pred = y_pred)
 
+	with open('prueba.csv', 'w', newline='') as file:
+		writer = csv.writer(file)
+		for i in range(len(files_names)):
+			writer.writerow([files_names[i], confidences[i]])
+
+
 	return ser
+
+def confidence_rate(batch_posteriorgram, input_length):
+	for it_batch in range(len(batch_posteriorgram)):
+		#Performing Best Path decoding:
+		posteriorgram = batch_posteriorgram[it_batch]
+
+		total_timesteps = len(posteriorgram[:input_length[it_batch],:])
+		total_estimation = 0
+		
+		#Looping over temporal slices to analyze:
+		for array in posteriorgram[:input_length[it_batch],:]:
+			total_estimation += 1 + array.max()
+		
+		mean_estimation = total_estimation / total_timesteps
+
+	return mean_estimation
 
 
 if __name__ == '__main__':
